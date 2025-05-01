d7a492fb948b238b2c55de52241e3a7fa9992f77
Kostkolama99 <matej.franek@gmail.com>
2025-05-01 19:54:22 +0200

    modified:   GTO.py 	modified:   templates/index.html 	new file:   templates/vysledek.html

--- GTO.py
+++ GTO.py
@@ -1,110 +1,110 @@
-from flask import Flask, render_template, request
-import random
-import matplotlib
-matplotlib.use('Agg')
+from flask import Flask, render_template, request, redirect, url_for, session
 import matplotlib.pyplot as plt
-import io
-import base64
+import os
+import random
 
 app = Flask(__name__)
+app.secret_key = 'tajny_klic'  # nastav pro session
 
-@app.route("/", methods=["GET", "POST"])
-def hra():
-    vysledky_text = ""
-    matice_text = ""
-    img_data = ""
+@app.route('/')
+def index():
+    session.clear()
+    session['score_1'] = 0
+    session['score_2'] = 0
+    session['tah'] = 0
+    session['prubeh_1'] = []
+    session['prubeh_2'] = []
+    session['log'] = []
+    session['zaznamy'] = {
+        'spoluprace': 0,
+        'uspesna_zrada_hrace_1': 0,
+        'uspesna_zrada_hrace_2': 0,
+        'nespoluprace': 0,
+        'zrada_1': 0,
+        'zrada_2': 0,
+        'spoluprace_1': 0,
+        'spoluprace_2': 0
+    }
+    session['volba_2'] = 'S'
+    return render_template('index.html')
 
-    if request.method == "POST":
-        score_hrace_1 = 0
-        score_hrace_2 = 0
-        spoluprace = 0
-        uspesna_zrada_hrace_1 = 0
-        uspesna_zrada_hrace_2 = 0
-        nespoluprace = 0
-        zrada_1 = 0
-        zrada_2 = 0
-        spoluprace_1 = 0
-        spoluprace_2 = 0
-        tahy = 10
-        prubeh_1 = []
-        prubeh_2 = []
-        volba_hrace_2 = "S"
+@app.route('/tah', methods=['POST'])
+def tah():
+    if session['tah'] >= 10:
+        return redirect(url_for('vysledek'))
 
-        for tah in range(tahy):
-            volba_hrace_1 = random.choice(["S", "Z"])
-            minula_volba_hrace_1 = volba_hrace_1
-            if tah > 0:
-                volba_hrace_2 = minula_volba_hrace_1
-                if random.random() < 0.1:
-                    volba_hrace_2 = "S" if volba_hrace_1 == "Z" else "Z"
+    volba_1 = request.form.get('volba')
+    if volba_1 not in ['S', 'Z']:
+        return redirect(url_for('index'))
 
-            if volba_hrace_1 == "S" and volba_hrace_2 == "S":
-                score_hrace_1 += 3
-                score_hrace_2 += 3
-                spoluprace += 1
-                zrada_2 += 1
-                spoluprace_1 += 1
-            elif volba_hrace_1 == "S" and volba_hrace_2 == "Z":
-                score_hrace_2 += 5
-                uspesna_zrada_hrace_2 += 1
-                zrada_2 += 1
-                spoluprace_1 += 1
-            elif volba_hrace_1 == "Z" and volba_hrace_2 == "S":
-                score_hrace_1 += 5
-                uspesna_zrada_hrace_1 += 1
-                zrada_1 += 1
-                spoluprace_2 += 1
-            else:
-                score_hrace_1 += 1
-                score_hrace_2 += 1
-                nespoluprace += 1
-                zrada_1 += 1
-                zrada_2 += 1
-
-            prubeh_1.append(score_hrace_1)
-            prubeh_2.append(score_hrace_2)
-
-        def pct(x): return f"{round(x / tahy * 100)} %"
-
-        vysledky_text = (
-            f"Skóre po {tahy} tazích:<br><br>"
-            f"Hráč 1: {score_hrace_1} bodů<br>"
-            f"Hráč 2: {score_hrace_2} bodů<br>"
-            f"Suma bodů: {score_hrace_1 + score_hrace_2}<br><br>"
-        )
-        if score_hrace_1 > score_hrace_2:
-            vysledky_text += "Hráč 1 vyhrál!<br>"
-        elif score_hrace_1 < score_hrace_2:
-            vysledky_text += "Hráč 2 vyhrál!<br>"
+    tah = session['tah']
+    volba_2 = session['volba_2']
+    if tah > 0:
+        if random.random() < 0.1:
+            volba_2 = 'Z' if session['volba_2'] == 'S' else 'S'
         else:
-            vysledky_text += "Je to remíza!<br>"
+            volba_2 = volba_1
+    session['volba_2'] = volba_2
 
-        matice_text = (
-            f"<pre>{'':15}| {'Hráč 2: S':>10} | {'Hráč 2: Z':>10} | {'Součet':>10}\n"
-            f"{'-'*52}\n"
-            f"{'Hráč 1: S':15}| {pct(spoluprace):>10} | {pct(uspesna_zrada_hrace_2):>10} | {pct(spoluprace_1):>10}\n"
-            f"{'Hráč 1: Z':15}| {pct(uspesna_zrada_hrace_1):>10} | {pct(nespoluprace):>10} | {pct(zrada_1):>10}\n"
-            f"{'-'*52}\n"
-            f"{'Součet':15}| {pct(spoluprace_2):>10} | {pct(zrada_2):>10} | {'100 %':>10}</pre>"
-        )
+    s = session['zaznamy']
 
-        fig, ax = plt.subplots(figsize=(9, 4))
-        ax.plot(prubeh_1, label="Hráč 1", color="blue")
-        ax.plot(prubeh_2, label="Hráč 2", color="red")
-        ax.set_xlabel("Tah")
-        ax.set_ylabel("Skóre")
-        ax.set_title("Vývoj skóre hráčů")
-        ax.legend()
-        ax.grid(True)
+    if volba_1 == 'S' and volba_2 == 'S':
+        session['score_1'] += 3
+        session['score_2'] += 3
+        s['spoluprace'] += 1
+        s['zrada_2'] += 1
+        s['spoluprace_1'] += 1
+        s['spoluprace_2'] += 1
+        zprava = "Oba spolupracovali."
+    elif volba_1 == 'S' and volba_2 == 'Z':
+        session['score_2'] += 5
+        s['uspesna_zrada_hrace_2'] += 1
+        s['zrada_2'] += 1
+        s['spoluprace_1'] += 1
+        zprava = "Hráč 2 tě zradil."
+    elif volba_1 == 'Z' and volba_2 == 'S':
+        session['score_1'] += 5
+        s['uspesna_zrada_hrace_1'] += 1
+        s['zrada_1'] += 1
+        s['spoluprace_2'] += 1
+        zprava = "Zradil jsi hráče 2."
+    else:
+        session['score_1'] += 1
+        session['score_2'] += 1
+        s['nespoluprace'] += 1
+        s['zrada_1'] += 1
+        s['zrada_2'] += 1
+        zprava = "Oba zradili."
 
-        buf = io.BytesIO()
-        plt.tight_layout()
-        plt.savefig(buf, format='png')
-        buf.seek(0)
-        img_data = base64.b64encode(buf.read()).decode('utf-8')
-        buf.close()
+    session['prubeh_1'].append(session['score_1'])
+    session['prubeh_2'].append(session['score_2'])
+    session['log'].append((tah + 1, volba_1, volba_2, zprava))
+    session['tah'] += 1
 
-    return render_template("index.html", vysledky=vysledky_text, matice=matice_text, graf=img_data)
+    return redirect(url_for('index'))
 
-if __name__ == "__main__":
+@app.route('/vysledek')
+def vysledek():
+    tahy = 10
+    s = session['zaznamy']
+
+    def pct(x): return round(x / tahy * 100)
+
+    fig, ax = plt.subplots()
+    ax.plot(session['prubeh_1'], label="Hráč 1", color="blue")
+    ax.plot(session['prubeh_2'], label="Hráč 2", color="red")
+    ax.set_xlabel("Tah")
+    ax.set_ylabel("Skóre")
+    ax.set_title("Vývoj skóre")
+    ax.legend()
+    ax.grid(True)
+
+    path = os.path.join('static', 'graf.png')
+    fig.savefig(path)
+    plt.close()
+
+    return render_template('vysledek.html', zaznamy=s, tahy=tahy, path=path,
+                           score_1=session['score_1'], score_2=session['score_2'])
+
+if __name__ == '__main__':
     app.run(debug=True)
